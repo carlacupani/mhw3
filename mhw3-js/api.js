9a89d10badfe504e6f813ad03f8a33d3cdcfd32f
Carla Cupani <cpnclp00@gmail.com>
2024-05-02 03:12:48 +0200

    committed

---
+++ mhw3-js/api.js
@@ -0,0 +1,11 @@
+'use strict';
+const api_key = 'd78d423f56d4447b1dde96e58bf54216';
+const imageBaseURL = 'https://image.tmdb.org/t/p/';
+
+const fetchDataFromServer = function (url, callback, optionalParam) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => callback(data, optionalParam));
+}
+
+export { imageBaseURL, api_key, fetchDataFromServer };
